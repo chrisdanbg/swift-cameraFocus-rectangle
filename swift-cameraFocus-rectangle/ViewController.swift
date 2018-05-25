12a9073ae8f0e4576cd0c4b419e750056b8e65e5
Kristiyan Danailov <chrisdanbg@icloud.com>
2018-05-25 18:57:46 +0300

    Initial Commit

---
+++ swift-cameraFocus-rectangle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-cameraFocus-rectangle
+//
+//  Created by Kristyan Danailov on 25.05.18 г..
+//  Copyright © 2018 г. Kristyan Danailov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
